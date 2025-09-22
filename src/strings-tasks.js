536e958e6643f58b747a74d60300316ca8e7c009
mashakozak96 <mashakozak96@gmail.com>
2025-09-22 15:53:42 +0300

    feat: add findLongestWord task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -341,8 +341,9 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const arr = sentence.split(' ').map((numArr) => numArr.length);
+  return sentence.split(' ')[arr.indexOf(Math.max(...arr))];
 }
 
 /**
