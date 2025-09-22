8fb96158a66c8cd70bb5e4343d0db388e1a0851d
mashakozak96 <mashakozak96@gmail.com>
2025-09-22 10:32:36 +0300

    feat: add sumOfCodes task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -184,8 +184,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let sum = 0;
+  if (typeof str !== 'string') {
+    return 0;
+  }
+  for (let i = 0; i < str.length; i += 1) {
+    sum += str.charCodeAt(i);
+  }
+  return sum;
 }
 
 /**
