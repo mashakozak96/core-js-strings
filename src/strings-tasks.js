88d3de476e6ec24290690c6ce1eddd3ea0ac4eaa
mashakozak96 <mashakozak96@gmail.com>
2025-10-04 21:59:46 +0300

    feat: add invertCase task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -372,8 +372,16 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((item) => {
+      if (item === item.toLowerCase()) {
+        return item.toUpperCase();
+      }
+      return item.toLowerCase();
+    })
+    .join('');
 }
 
 /**
